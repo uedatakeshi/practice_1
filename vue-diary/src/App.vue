b1a0e362206486718b9efc75ea136ace52fb7ec2
Takeshi Ueda <uedatakeshi@gmail.com>
2022-10-01 16:02:36 +0900

    add vue diary

---
+++ vue-diary/src/App.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import DiaryForm from "./components/DiaryForm.vue";
+import DiaryView from "./components/DiaryView.vue";
+
+const diary = ref({
+  weather:"雨",
+    manager:"山田",
+    sleepTime:9,
+    start:"10:00",
+    end:"17:00",
+    comment:"はじめまして。"
+});
+
+</script>
+
+
+<template>
+  <div class="App">
+    <h2>2022年09月22日の日記</h2>
+    <div>
+      <div><button class='btn btn-primary mb-2'>戻る</button></div>
+      <DiaryForm />
+    </div>
+  </div>
+  <div>
+    <div><button class='btn btn-success mb-2'>編集する</button></div>
+    <DiaryView v-bind:diary="diary" />
+  </div>
+
+
+
+  <div class="wrapper">
+    <HelloWorld msg="You did it!" />
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ vue-diary/src/components/DiaryForm.vue
@@ -0,0 +1,61 @@
+<template>
+            <div class="mb-3">
+                <label class="form-label" for='weather'>天気</label>
+                <div class="row">
+                    <div class="col-auto">
+                        <select class="form-control form-select-lg" id="weather" name="weather" >
+                            <option>晴れ</option>
+                            <option>曇り</option>
+                            <option>雨</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="manager">担当者</label>
+                <div class="row">
+                    <div class="col-auto">
+                        <select class="form-control form-select-lg" id="manager" name="manager">
+                            <option>山田</option>
+                            <option>前田</option>
+                            <option>高田</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="sleep_time">睡眠時間(h)</label>
+                <div class="row">
+                    <div class="col-auto">
+                        <input type="number" class="form-control" id="sleep_time" name="sleep_time" />
+                    </div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="start">出社時間</label>
+                <div class="row">
+                    <div class="col-auto">
+                        <input type="time" class="form-control" id="start" name="start" />
+                    </div>
+                    <div class="col-auto">
+                        ～
+                    </div>
+                    <div class="col-auto">
+                        <input type="time" class="form-control" id="end" name="end" />
+                    </div>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label class="form-label" for="comment">コメント</label>
+                <textarea class="form-control" id="comment" name="comment" ></textarea>
+            </div>
+            <div class="row">
+                <div class="col-auto">
+                    <button type="button" class="btn btn-primary">登録する</button>
+                </div>
+                <div class="col-auto">
+                    <button type="button" class="btn  btn-outline-secondary">削除する</button>
+                </div>
+            </div>
+
+</template>
