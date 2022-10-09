959fcef0f8be1ba454162c1df2d33dcb8a4c7dcd
Takeshi Ueda <uedatakeshi@gmail.com>
2022-10-09 11:27:48 +0900

    run build

--- vue-diary/vite.config.ts
+++ vue-diary/vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: './',
+  build: {
+    outDir: 'diary',
+    indexPath: 'index_bundled.html'
+  }
 });
